9525ac27a0535f2d41bb15379aef21dc44818eaf
Dhamodharan C <dhamoeee2@gmail.com>
2024-04-25 07:58:05 +0530

    commit chnged

--- src/Controller/controller.js
+++ src/Controller/controller.js
@@ -1,16 +1,1 @@
-
-// const express = require('express');
-
-
-// const app = express();
-
-
-// app.get('/', (req, res) => {
-//     res.send('Hello, world! This is a GET request.');
-// });
-
-
-// const PORT = process.env.PORT || 3002;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
+console.log("hai its dhamodharan")

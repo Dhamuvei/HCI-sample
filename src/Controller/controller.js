bf44831e8b5e00ec55283921edbec369af07dce0
Dhamodharan C <dhamoeee2@gmail.com>
2024-04-25 07:55:07 +0530

    commented chase file

--- src/Controller/controller.js
+++ src/Controller/controller.js
@@ -1,1 +1,16 @@
-console.log("hai its dhamu")
+
+// const express = require('express');
+
+
+// const app = express();
+
+
+// app.get('/', (req, res) => {
+//     res.send('Hello, world! This is a GET request.');
+// });
+
+
+// const PORT = process.env.PORT || 3002;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
